d51a557de49512b99ec8dfb03e7917ff2566612a
Rafael Gagliardi <frc.rafael@gmail.com>
2025-04-04 16:46:12 -0300

    Corrigindo

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python app.py
+gunicorn app:app --bind 0.0.0.0:8000
--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import flask
-app = Flask (__name__)
+from flask import Flask
+
+app = Flask(__name__)
+
 @app.route("/")
 def home():
-  return "Olá Rafael! Sua API está no ar"
+    return "Olá Rafael! Sua API está no ar"
 
-if __name__ == "main":
-  app.run(host="0.0.0.0",port=8000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
